77d0daaa7f9609fe97f2649626995083660a6c05
JesusDavCal <jezus97david@gmail.com>
2026-09-17 18:53:47 -0500

    index.tsx

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,13 +1,22 @@
+import { Link } from "expo-router";
 import { StyleSheet } from "react-native";
 export default function App (){
   return(
     <view style={styles.container}>
       <text style={styles.text}> Asistencia </text>
+      <Link href="/about" style={styles.button}>
+      Go To About Us
+      </Link>
     </view>
   )
 }
 
 const styles = StyleSheet.create ({
+  button:{
+   fontSize: 18,
+   color: "fff",
+   backgroundColor: "#415252",
+  },
   container: {
     flex:1,
     justifyContent:"center",
